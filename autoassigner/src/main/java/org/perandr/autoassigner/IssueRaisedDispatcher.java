a17edf0c632af6688bf01b80afc7b5e792125f15
Andrew <perandr@gmail.com>
2013-12-15 16:16:47 +0200

    new issue

--- autoassigner/src/main/java/org/perandr/autoassigner/IssueRaisedDispatcher.java
+++ autoassigner/src/main/java/org/perandr/autoassigner/IssueRaisedDispatcher.java
@@ -2,16 +2,32 @@ package org.perandr.autoassigner;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.sonar.api.issue.Issue;
+import org.sonar.api.issue.IssueHandler;
 import org.sonar.api.notifications.Notification;
 import org.sonar.api.notifications.NotificationDispatcher;
 
-public class IssueRaisedDispatcher extends NotificationDispatcher {
+public class IssueRaisedDispatcher extends NotificationDispatcher implements IssueHandler{
 	private static final Logger LOG = LoggerFactory.getLogger(IssueRaisedDispatcher.class);
 	
 	@Override
-	public void dispatch(Notification notification, Context context) {
+	public void dispatch(Notification notification, NotificationDispatcher.Context context) {
 		
 		LOG.info("==================================\r\n{}",notification.getType());
+		
 	}
+
+	public void onIssue(org.sonar.api.issue.IssueHandler.Context context) {
+		if(context.isNew()){
+			Issue issue = context.issue();
+			LOG.info("====================== ISSSSUUUUUEEEEEEEEEE",issue.message());
+			LOG.info("Message {}",issue.message());
+			LOG.info("assignee {}",issue.assignee());
+			LOG.info("authorLogin {}",issue.authorLogin());
+			LOG.info("creationDate {}",issue.creationDate());
+			LOG.info("creationDate {}",issue.ruleKey());
+		}
+	}
+
 
 }
