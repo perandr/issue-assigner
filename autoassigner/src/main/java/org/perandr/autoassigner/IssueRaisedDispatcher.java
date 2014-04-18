154c297aeaf973597e40e465563ac080a06ec56c
Andrew <perandr@gmail.com>
2014-04-18 08:55:05 +0300

    Implemented major logic for assigning issue to author of it

--- autoassigner/src/main/java/org/perandr/autoassigner/IssueRaisedDispatcher.java
+++ autoassigner/src/main/java/org/perandr/autoassigner/IssueRaisedDispatcher.java
@@ -7,114 +7,220 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.sonar.api.database.DatabaseSession;
+import org.sonar.api.database.model.MeasureData;
+import org.sonar.api.database.model.MeasureModel;
 import org.sonar.api.issue.Issue;
 import org.sonar.api.issue.IssueHandler;
 import org.sonar.api.measures.CoreMetrics;
+import org.sonar.api.measures.FileLinesContext;
+import org.sonar.api.measures.FileLinesContextFactory;
 import org.sonar.api.measures.Measure;
 import org.sonar.api.measures.Metric;
 import org.sonar.api.notifications.Notification;
 import org.sonar.api.notifications.NotificationDispatcher;
 import org.sonar.api.platform.ComponentContainer;
+import org.sonar.api.qualitymodel.Characteristic;
+import org.sonar.api.qualitymodel.CharacteristicProperty;
 import org.sonar.api.resources.JavaFile;
 import org.sonar.api.resources.ProjectFileSystem;
 import org.sonar.api.resources.Resource;
 import org.sonar.api.rule.RuleKey;
 
-public class IssueRaisedDispatcher extends NotificationDispatcher implements IssueHandler{
-	private static final Logger LOG = LoggerFactory.getLogger(IssueRaisedDispatcher.class);
-	private ProjectFileSystem projectFileSystem;
+public class IssueRaisedDispatcher extends NotificationDispatcher implements
+		IssueHandler {
+	private static final Logger LOG = LoggerFactory
+			.getLogger(IssueRaisedDispatcher.class);
+
 	private ComponentContainer componentContainer;
-	  private DatabaseSession session;
+	private DatabaseSession session;
 	
-	public IssueRaisedDispatcher(ComponentContainer componentContainer,  DatabaseSession session) {
+	public IssueRaisedDispatcher(ComponentContainer componentContainer,
+			DatabaseSession session) {
 		this.componentContainer = componentContainer;
-		this.session =session;
+		this.session = session;
 	}
+
 	@Override
-	public void dispatch(Notification notification, NotificationDispatcher.Context context) {
-		
-		LOG.info("==================================\r\n{}",notification.getType());
-		LOG.info("DefaultMessage {}",notification.getDefaultMessage());
+	public void dispatch(Notification notification,
+			NotificationDispatcher.Context context) {
+
+		LOG.info("==================================\r\n{}",
+				notification.getType());
+		LOG.info("DefaultMessage {}", notification.getDefaultMessage());
 		LOG.info("toString : ", notification.toString());
 	}
 
 	public void onIssue(org.sonar.api.issue.IssueHandler.Context context) {
-		if(context.isNew()){
+		if (context.isNew()) {
 			Issue issue = context.issue();
-			LOG.info("====================== ISSSSUUUUUEEEEEEEEEE",issue.message());
-			LOG.info("Message {}",issue.message());
-			LOG.info("assignee {}",issue.assignee());
-			LOG.info("authorLogin {}",issue.authorLogin());
-			LOG.info("creationDate {}",issue.creationDate());
-			LOG.info("ruleKey {}",issue.ruleKey());
-			LOG.info("rule Line {}",issue.line());
-			LOG.info("actionPlanKey {}",issue.actionPlanKey());
-			LOG.info("componentKey {}",issue.componentKey());
-			LOG.info("severity{}",issue.severity());
+			LOG.info("====================== ISSSSUUUUUEEEEEEEEEE",
+					issue.message());
+			LOG.info("Message {}", issue.message());
+			LOG.info("assignee {}", issue.assignee());
+			LOG.info("authorLogin {}", issue.authorLogin());
+			LOG.info("creationDate {}", issue.creationDate());
+			LOG.info("ruleKey {}", issue.ruleKey());
+			LOG.info("rule Line {}", issue.line());
+			LOG.info("actionPlanKey {}", issue.actionPlanKey());
+			LOG.info("componentKey {}", issue.componentKey());
+			LOG.info("severity{}", issue.severity());
 			RuleKey ruleKey = issue.ruleKey();
-			LOG.info("ruleKey.repository{}",ruleKey.repository());
-			LOG.info("ruleKey.rule{}",ruleKey.rule());
-			
+			LOG.info("ruleKey.repository{}", ruleKey.repository());
+			LOG.info("ruleKey.rule{}", ruleKey.rule());
+
 			LOG.info("attributes");
 			Map<String, String> attributes = issue.attributes();
-			for(String attribute : attributes.keySet()){
-				LOG.info("Key{}, Value{}",attribute,attributes.get(attribute));
+			if (attributes != null && !attributes.isEmpty()) {
+				for (String attribute : attributes.keySet()) {
+					LOG.info("Key{}, Value{}", attribute,
+							attributes.get(attribute));
+				}
+			} else {
+				LOG.info("!! attributes are empty !!");
 			}
-			
-			
-			
-			if(projectFileSystem!=null){
-			LOG.info("===File");
-			File file = projectFileSystem.resolvePath(issue.componentKey());
-			if(file!=null){
-				Resource resource = projectFileSystem.toResource(file);
-				dumpResource(resource);
-			}
-			}
-			
-			LOG.info("===JavaFile");
+
+//			if (projectFileSystem != null) {
+//				LOG.info("===File");
+//				File file = projectFileSystem.resolvePath(issue.componentKey());
+//				if (file != null) {
+//					Resource resource = projectFileSystem.toResource(file);
+//					dumpResource(resource);
+//				}
+//			}
+
 			JavaFile jf = new JavaFile(issue.componentKey());
-			if(jf!=null){
-				  Metric measure = getSCMMesure(jf);
+			if (jf != null) {
+				LOG.info("===JavaFile");
+				//Metric measure = getSCMMesure(context, jf);
 				dumpResource(jf);
-				
 			}
-			
-			LOG.info("===Component");
-			Object component = componentContainer.getComponentByKey(issue.componentKey());
-			if(component!=null){
-			if(component instanceof Resource){
-				dumpResource((Resource)component);
+
+			Object component = componentContainer.getComponentByKey(issue
+					.componentKey());
+			if (component != null) {
+				LOG.info("===Component");
+				if (component instanceof Resource) {
+					LOG.info("===Component == Resource");
+					dumpResource((Resource) component);
+				}
 			}
-			}
-			
-			
+
 		}
 	}
-	
-	private Metric getSCMMesure(JavaFile jf) {
-		//CoreMetrics.SCM_AUTHORS_BY_LINE
-		Object criterias;
-		 List<Metric> metrics = session.getResults(Metric.class, criterias);
-		return metrics.get(0);
+
+	private Metric getSCMMesure(
+			org.sonar.api.issue.IssueHandler.Context context, JavaFile jf) {
+		// CoreMetrics.SCM_AUTHORS_BY_LINE
+		List<Metric> metrics = session.getResults(Metric.class);
+
+		//dumpMetric(metrics);
+
+		List<MeasureModel> measures = session.getResults(MeasureModel.class);
+		dumpMesures(measures);
+
+		// return metrics.get(0);
+		return null;
 	}
+
+	private void dumpMetric(List<Metric> metrics) {
+		LOG.info("========== METRICS ===========");
+		for (Metric metric : metrics) {
+			LOG.info(String.format("Class [%s], Key %s, Name %s, Origin %s",
+					metric.getClass(), metric.getKey(), metric.getName(),
+					metric.getOrigin()));
+			if (CoreMetrics.SCM_AUTHORS_BY_LINE.equals(metric.getKey())) {
+				LOG.info("!!!!!!!!!! FOUND METRIC"
+						+ CoreMetrics.SCM_AUTHORS_BY_LINE);
+			}
+
+		}
+	}
+
+	private void dumpMesures(List<MeasureModel> measures) {
+		LOG.info("========== MEASURES ===========");
+		for (MeasureModel measure : measures) {
+			String str = "";
+			StringBuilder sb = new StringBuilder(1024);
+			MeasureData measureData = measure.getMeasureData();
+			if (measureData != null) {
+				str = String.format("Data.Test: [%s], Data [%s],",
+						measureData.getText(), measureData.getData());
+			}
+
+			sb.append("Class: [");
+			sb.append(measure.getClass());
+			sb.append("], ");
+			sb.append("AlertText: [");
+			sb.append(measure.getAlertText());
+			sb.append("], ");
+			sb.append("Url: [");
+			sb.append(measure.getUrl());
+			sb.append("], ");
+			sb.append("Description: [");
+			sb.append(measure.getDescription());
+			sb.append("], ");
+
+			sb.append("TextValue: [");
+			sb.append(measure.getTextValue());
+			sb.append("], ");
+			sb.append("Value: [");
+			sb.append(measure.getValue());
+			sb.append("], ");
+			sb.append("MesureData: [");
+			sb.append(str);
+			sb.append("], ");
+
+			//dump(measure.getCharacteristic(), sb);
+			LOG.info(sb.toString());
+		}
+	}
+
+	private void dump(Characteristic characteristic, StringBuilder sb) {
+		// if (characteristic != null) {
+		// sb.append("Characteristic: \r\n");
+		// sb.append("Key: [");
+		// sb.append(characteristic.getKey());
+		// sb.append("], ");
+		// sb.append("Description: [");
+		// sb.append(characteristic.getDescription());
+		// sb.append("], ");
+		// sb.append("Name: [");
+		// sb.append(characteristic.getName());
+		// sb.append("], ");
+		// sb.append("\r\n\t\tProperties: [");
+		// for (CharacteristicProperty prop : characteristic.getProperties()) {
+		// sb.append("[");
+		// sb.append(prop.getKey());
+		// sb.append(", ");
+		// sb.append(prop.getTextValue());
+		// sb.append("], ");
+		// }
+		// sb.append("], ");
+		// List<Characteristic> children = characteristic.getChildren();
+		// if (children != null && !children.isEmpty()) {
+		// for (Characteristic child : characteristic.getChildren()) {
+		// dump(child, sb);
+		// }
+		// }
+		// }
+	}
+
 	public static void dumpResource(Resource resource) {
 		LOG.info("--------------------------- RESOURCE -----------------------");
-		LOG.info("ID {}",resource.getId());
-		LOG.info("Key {}",resource.getKey());
-		LOG.info("EffectiveKey {}",resource.getEffectiveKey());
+		LOG.info("ID {}", resource.getId());
+		LOG.info("Key {}", resource.getKey());
+		LOG.info("EffectiveKey {}", resource.getEffectiveKey());
 
-		LOG.info("Name {}",resource.getName());
-		LOG.info("LongName {}",resource.getLongName());
-		LOG.info("Description {}",resource.getDescription());
-		
-		LOG.info("Qualifier {}",resource.getQualifier());
-		LOG.info("Scope {}",resource.getScope());
-		LOG.info("Parent {}",resource.getParent());
-		LOG.info("Language {}",resource.getLanguage());
-		
-		LOG.info("------------------------------------------------------------");		
+		LOG.info("Name {}", resource.getName());
+		LOG.info("LongName {}", resource.getLongName());
+		LOG.info("Description {}", resource.getDescription());
+
+		LOG.info("Qualifier {}", resource.getQualifier());
+		LOG.info("Scope {}", resource.getScope());
+		LOG.info("Parent {}", resource.getParent());
+		LOG.info("Language {}", resource.getLanguage());
+
+		LOG.info("------------------------------------------------------------");
 	}
-
 
 }
--- autoassigner/src/main/java/org/perandr/autoassigner/batch/ExampleSensor.java
+++ autoassigner/src/main/java/org/perandr/autoassigner/batch/ExampleSensor.java
@@ -1,7 +1,14 @@
 package org.perandr.autoassigner.batch;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import javax.persistence.Query;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.map.HashedMap;
+import org.apache.commons.lang.StringUtils;
 import org.perandr.autoassigner.ExampleMetrics;
 import org.perandr.autoassigner.AutoassignerPlugin;
 import org.slf4j.Logger;
@@ -9,52 +16,148 @@ import org.slf4j.LoggerFactory;
 import org.sonar.api.batch.Sensor;
 import org.sonar.api.batch.SensorContext;
 import org.sonar.api.config.Settings;
+import org.sonar.api.database.DatabaseSession;
+import org.sonar.api.database.model.MeasureData;
+import org.sonar.api.database.model.MeasureModel;
+import org.sonar.api.database.model.User;
+import org.sonar.api.issue.Issue;
+import org.sonar.api.issue.IssueHandler;
+import org.sonar.api.measures.FileLinesContext;
+import org.sonar.api.measures.FileLinesContextFactory;
 import org.sonar.api.measures.Measure;
+import org.sonar.api.resources.JavaFile;
 import org.sonar.api.resources.Project;
 import org.sonar.api.rules.Violation;
+import org.sonar.api.utils.KeyValueFormat;
 
-public class ExampleSensor implements Sensor {
+public class ExampleSensor implements Sensor, IssueHandler {
 
-  private static final Logger LOG = LoggerFactory.getLogger(ExampleSensor.class);
+	private static final Logger LOG = LoggerFactory
+			.getLogger(ExampleSensor.class);
 
-  private Settings settings;
+	public static final String SCM_AUTHORS_BY_LINE_KEY = "authors_by_line";
+	private Settings settings;
 
-  /**
-   * Use of IoC to get Settings
-   */
-  public ExampleSensor(Settings settings) {
-    this.settings = settings;
-    //dumpSettings(settings);
-  }
+	private DatabaseSession session;
+	FileLinesContextFactory fileLinesContextFactory;
 
-  private void dumpSettings(Settings settings2) {
-	Map<String, String> properties = settings.getProperties();
-	for(String propKey : properties.keySet()){
-		LOG.info("Key {}, Value {}", propKey,properties.get(propKey));
+	/**
+	 * Use of IoC to get Settings
+	 */
+	public ExampleSensor(Settings settings,
+			FileLinesContextFactory fileLinesContextFactory,
+			DatabaseSession session) {
+		this.settings = settings;
+		this.fileLinesContextFactory = fileLinesContextFactory;
+		this.session = session;
+		// dumpSettings(settings);
 	}
-}
 
-public boolean shouldExecuteOnProject(Project project) {
-    // This sensor is executed on any type of projects
-    return true;
-  }
+	private void dumpSettings(Settings settings2) {
+		Map<String, String> properties = settings.getProperties();
+		for (String propKey : properties.keySet()) {
+			LOG.info("Key {}, Value {}", propKey, properties.get(propKey));
+		}
+	}
 
-  public void analyse(Project project, SensorContext sensorContext) {
-    String value = settings.getString(AutoassignerPlugin.SUBJECT_TEMPLATE_PROPERTY);
-    LOG.info(AutoassignerPlugin.SUBJECT_TEMPLATE_PROPERTY + "=" + value);
-    LOG.info("============Date: {} ",project.getAnalysisDate());
-    LOG.info("============IsLatest: {} ",project.isLatestAnalysis());
-    
-//    LOG.info("============Date: {} ",);
-//    Violation violation = Violation.;
-//	sensorContext.saveViolation(violation )
-//    //    Measure measure = new Measure(ExampleMetrics.MESSAGE, value);
-//    sensorContext.saveMeasure(measure);
-  }
+	public boolean shouldExecuteOnProject(Project project) {
+		// This sensor is executed on any type of projects
+		return true;
+	}
 
-  @Override
-  public String toString() {
-    return getClass().getSimpleName();
-  }
+	public void analyse(Project project, SensorContext sensorContext) {
+		String value = settings
+				.getString(AutoassignerPlugin.SUBJECT_TEMPLATE_PROPERTY);
+		LOG.info(AutoassignerPlugin.SUBJECT_TEMPLATE_PROPERTY + "=" + value);
+		LOG.info("============Date: {} ", project.getAnalysisDate());
+		LOG.info("============IsLatest: {} ", project.isLatestAnalysis());
+
+		// LOG.info("============Date: {} ",);
+		// Violation violation = Violation.;
+		// sensorContext.saveViolation(violation )
+		// // Measure measure = new Measure(ExampleMetrics.MESSAGE, value);
+		// sensorContext.saveMeasure(measure);
+	}
+
+	public void onIssue(Context context) {
+		if (context.isNew()) {
+			Issue issue = context.issue();
+			JavaFile jf = new JavaFile(issue.componentKey());
+			if (jf != null) {
+				LOG.info("ResourceID {}", jf.getId());
+				Query query = session
+						.getEntityManager()
+						.createQuery(
+								"select mm from MeasureModel as mm, "
+										+ "\r\n Metric as m where m.key='authors_by_line' and m.id=mm.metricId");
+				List<MeasureModel> result = query.getResultList();
+				for (MeasureModel measure : result) {
+					LOG.info("==========" + measure.getId()
+							+ "==================== ");
+					MeasureData data = measure.getMeasureData();
+					Map<String, String> values = KeyValueFormat.parse(data
+							.getText());
+					dump(values);
+					String author = values.get(String.valueOf(issue.line()));
+					if (StringUtils.isNotBlank(author)) {
+						User user = getUser(author);
+						context.setAuthorLogin(user.getLogin());
+						context.assign(user.getLogin());
+						LOG.info("======================================== ");
+						LOG.info("Author: " + author + "==================== ");
+						LOG.info("======================================== ");
+					}
+				}
+
+			}
+		}
+	}
+
+	private User getUser(String author) {
+		User user; 
+		Query userQuery = session
+				.getEntityManager()
+				.createQuery(
+						"select u from User as u where u.login =:login or u.email=:login");
+		userQuery.setParameter("login", author);
+		List<User> users = userQuery.getResultList();
+		
+		if(CollectionUtils.isEmpty(users)){
+			user = createUser(author);
+		}else{
+			user = users.get(0);
+		}
+		return user;
+	}
+
+	private User createUser(String author) {
+		User user = new User();
+		user.setLogin(author);
+		if(isEmail(author)){
+			user.setEmail(author);
+		}
+		session
+		.getEntityManager().persist(user);
+		session
+		.getEntityManager().flush();
+		return user;
+	}
+
+	private boolean isEmail(String author) {		
+		return author.contains("@");
+	}
+
+	private void dump(Map<String, String> values) {
+		LOG.info("======================================== ");
+		for (String key : values.keySet()) {
+			LOG.info("Key {}, Value {}", key, values.get(key));
+		}
+		LOG.info("======================================== ");
+	}
+
+	@Override
+	public String toString() {
+		return getClass().getSimpleName();
+	}
 
 }
